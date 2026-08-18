a1b4b34f0be7e84cea209c9d4a2becbb88b3de2c
Tom Oddy <tom_oddy@live.co.uk>
2026-08-18 11:14:01 +0100

    Implemented OIDC login

--- SmarterMeterUI/Program.cs
+++ SmarterMeterUI/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.HttpOverrides;
 using SmarterMeterUI.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -6,23 +8,41 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<MeterService>();
-
-// Cookie authentication - issued on successful password login, checked on every request
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
     .AddCookie(options =>
     {
-        options.LoginPath = "/Login";
         options.ExpireTimeSpan = TimeSpan.FromDays(30);
         options.SlidingExpiration = true;
+    })
+    .AddOpenIdConnect(options =>
+    {
+        options.Authority = builder.Configuration["Oidc:Authority"];
+        options.ClientId = builder.Configuration["Oidc:ClientId"];
+        options.ClientSecret = builder.Configuration["Oidc:ClientSecret"];
+        options.ResponseType = "code";
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
     });
 builder.Services.AddAuthorization();
 
 WebApplication app = builder.Build();
-
 app.UseExceptionHandler("/Error");
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
+app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
 app.MapRazorPages().WithStaticAssets();
 app.Run();
